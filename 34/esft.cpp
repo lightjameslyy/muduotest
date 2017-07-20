88e4a5c04f6c0d1da83a0be48e096b0d4b7f7bd4
cassvin <ccc6710@163.com>
2017-07-20 20:57:03 +0800

    add

--- 34/esft.cpp
+++ 34/esft.cpp
@@ -18,16 +18,16 @@ public:
 
 int main()
 {
-	boost::shared_ptr<Y> p(new Y);
-	boost::shared_ptr<Y> q = p->f();
-
+	boost::shared_ptr<Y> p(new Y);//p的引用计数为1，
+	boost::shared_ptr<Y> q = p->f();//引用计数为2
+ 
 	Y* r = p->f2();
-	assert(p == q);
-	assert(p.get() == r);
+	assert(p == q);//正确
+	assert(p.get() == r);//p的指针（即new Y)=r
 
-	std::cout<<p.use_count()<<std::endl;
-	boost::shared_ptr<Y> s(r);
-	std::cout<<s.use_count()<<std::endl;
+	std::cout<<p.use_count()<<std::endl;//2
+	boost::shared_ptr<Y> s(r);//等于又构造了一个独立share_ptr对象,并不是把某个share_ptr对象赋值给另外一个，后者的引用计数才+1=3
+	std::cout<<s.use_count()<<std::endl;//1
 	assert(p == s);
 
 	return 0;
